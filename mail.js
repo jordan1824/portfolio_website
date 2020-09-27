1fa7080d225a4a492ffc85c02cd0ee0c1b082273
Jordan Deveau <jordandeveau@Jordans-Macbook.local>
2020-09-27 03:11:17 -0400

    Finished contact form & build process

--- mail.js
+++ mail.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer')
 const mailGun = require('nodemailer-mailgun-transport')
+const dotenv = require("dotenv")
+dotenv.config()
+
+let api_key = process.env.API_KEY
+let domain = process.env.DOMAIN
 
 const auth = {
   auth: {
-    api_key: 'bae008c826a3d10f248c9ca8e89eefab-cb3791c4-487a0ea7',
-    domain: 'sandbox1650025a850b4bed8b9b843f8feef20a.mailgun.org'
+    api_key: api_key,
+    domain: domain
   }
 }
 
